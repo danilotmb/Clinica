bb73a77dc7afdc4603d96c1c767627ecfda4fa27
Danilo Tambone <dtambone12@gmail.com>
2024-02-28 13:33:46 +0100

    Commit 7

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
 # Aggiungi i percorsi a composer e php
-export PATH=$PATH:/opt/homebrew/bin/composer
-export PATH=$PATH:/opt/homebrew/bin/php
+export PATH=$PATH:/opt/homebrew/bin
 
 echo "Running composer install..."
 composer install --no-dev
 
 echo "Caching config..."
-/opt/homebrew/bin/php artisan config:cache
+php artisan config:cache
 
 echo "Caching routes..."
-/opt/homebrew/bin/php artisan route:cache
+php artisan route:cache
 
 echo "Running migrations..."
-/opt/homebrew/bin/php artisan migrate --force
+php artisan migrate --force
 
 # Avvia nginx e php-fpm
-service nginx start
-service php-fpm start
+/opt/homebrew/etc/nginx/sbin/nginx
+/opt/homebrew/sbin/php-fpm
