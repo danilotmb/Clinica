a68212cd555276fc8c9ddc8214b49bc57a975ba7
Danilo Tambone <dtambone12@gmail.com>
2024-02-28 17:46:32 +0100

    Test deploy

---
+++ index.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Redirect Page</title>
+</head>
+<body>
+    <h1>Clicca il pulsante per il redirect</h1>
+    <form action="https://laravel-memcache-bbdj.onrender.com" method="get">
+        <button type="submit">Vai alla pagina</button>
+    </form>
+</body>
+</html>
