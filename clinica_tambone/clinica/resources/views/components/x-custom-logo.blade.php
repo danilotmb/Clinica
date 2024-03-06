d54b6d1b9c904796f9389285695af1af520e16be
Danilo Tambone <dtambone12@gmail.com>
2024-03-06 13:18:55 +0100

    Test

--- clinica_tambone/clinica/resources/views/components/x-custom-logo.blade.php
+++
@@ -1,3 +0,0 @@
-<div>
-    <img {{ $attributes->merge(['class' => '']) }} src="{{ asset('assets/img/logo/logo.jpg') }}" alt="Logo" style="max-width: 100%; height: auto;">
-</div>
--- clinica_tambone/clinica/resources/views/notifications/visit_reminder.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('vendor.notifications.email')
-
-@section('content')
-    <h2>{{ $title }}</h2>
-    <p>{{ $body }}</p>
-@endsection
